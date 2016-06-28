84be40037585f14a04723a2db25e181fe1a1d69a
Don Owino and Rich Zemke <pair+don+rich@allstate.com>
2016-06-28 16:32:24 -0500

    works

--- src/controllers/seat.js
+++ src/controllers/seat.js
@@ -5,7 +5,6 @@ import Seat from '../models/seat';
 const router = module.exports = express.Router();
 
 router.get('/', (req, res) => {
-  console.log('seat get');
   Seat.find().populate(['section']).exec((err, seats) => {
     res.send({ seats });
   });
@@ -14,10 +13,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   const purchased = req.body.purchased;
   const id = req.body.id;
-  console.log('seat post');
-  console.log(req.body);
   Seat.findByIdAndUpdate(id, { purchased }, (updateErr, seat) => {
-    console.log('update error', updateErr);
     res.send({ seat });
   });
   // seat.save(() => res.send({ seat }));
